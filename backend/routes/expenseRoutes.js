45372351ad6baf8041085421df0b45c7950082c5
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-10-11 22:52:06 +0530

    update expense

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addExpense,getExpense,deleteExpense } from "../controllers/expenseController.js";
+import { addExpense,getExpense,deleteExpense,updateExpense } from "../controllers/expenseController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router=express.Router();
@@ -7,5 +7,6 @@ const router=express.Router();
 router.post('/',protect,addExpense);
 router.get('/',protect,getExpense);
 router.delete('/:id',protect,deleteExpense);
+router.patch('/:id',protect,updateExpense);
 
 export default router;
