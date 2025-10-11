0fbf1dced9e0797ac193a0cc84ef83e96fd9fe71
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-10-11 22:51:46 +0530

    adding update expense route

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -21,7 +21,7 @@ export const addExpense=async(req,res)=>{
 
 export const getExpense=async(req,res)=>{
     try{
-        const expenses=await Expense.findById({user:req.user._id}).sort({date:-1});
+        const expenses=await Expense.find({user:req.user._id}).sort({date:-1});
         res.json(expenses);
     }
     catch(err){
@@ -40,6 +40,30 @@ export const deleteExpense=async(req,res)=>{
 
         await Expense.deleteOne();
         return res.status(200).json({message:"Expense deleted!"});
+    }
+    catch(err){
+        return res.status(500).json({message:err.message});
+    }
+};
+
+export const updateExpense=async(req,res)=>{
+    try{
+        const expense=await Expense.findById(req.params.id);
+        if(!expense){
+            return res.status(404).json({message:"No such expense"});
+        }
+
+        if(expense.user.toString() !== req.user._id.toString()){
+            return res.status(401).json({message:"Not authorized!"});
+        }
+
+        const updateExpense=await Expense.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+
+        return res.status(201).json({message:"Successfully updated the expense!"});
     }
     catch(err){
         return res.status(500).json({message:err.message});
