184149b8e4a1a430640323c0fdca7564fad67d6c
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-10-12 21:51:18 +0530

    Storing budgetLimit and phoneNumber

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,19 +3,19 @@ import jwt from "jsonwebtoken";
 
 export const signup=async(req,res)=>{
     try{
-        const {name,email,password}=req.body;
+        const {name,email,password,budgetLimit,phoneNumber}=req.body;
 
         const existingUser=await User.findOne({email});
         if(existingUser){
             return res.status(400).json({message:"User already exists!"});
         }
 
-        const user=new User({name,email,password});
+        const user=new User({name,email,password,budgetLimit,phoneNumber});
         await user.save();
 
         const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
 
-        res.status(201).json({user:{id:user._id,name:user.name,email:user.email},token});
+        res.status(201).json({user:{id:user._id,name:user.name,email:user.email,budgetLimit:user.budgetLimit,phoneNumber:user.phoneNumber},token});
     }
     catch(err){
         res.status(500).json({message:err.message});
