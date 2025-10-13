06333484f822993788b52124a7714816cb911d17
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-10-13 11:58:05 +0530

    file handling middlware in route

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addExpense,getExpense,deleteExpense,updateExpense,analyzeData,expensePDF } from "../controllers/expenseController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import upload from "../middleware/uploadMiddleware.js";
 
 const router=express.Router();
 
-router.post('/',protect,addExpense);
+router.post('/',protect,upload.single("receipt"),addExpense);
 router.get('/',protect,getExpense);
 router.delete('/:id',protect,deleteExpense);
 router.patch('/:id',protect,updateExpense);
