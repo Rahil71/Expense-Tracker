a6cb1d609c27699967b30b804a8ace9616e003f2
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-11-06 19:43:53 +0530

    Solving cors error

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import expenseRoutes from "./routes/expenseRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 
 app.use("/api/auth",authRoutes);
